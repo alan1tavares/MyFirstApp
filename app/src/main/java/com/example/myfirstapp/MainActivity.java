ba37ffca2ec895b498641c369374b6f12cdf8a05
Alan Tavares <alan1tavares@gmail.com>
2020-12-07 23:00:26 -0300

    :sparkles: Finalizado a parte "Criar um IU simples"
    Dados inicio a parte "Iniciar outra atividade"

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+
+    }
+
 }
